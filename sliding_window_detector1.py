ba53a43e989ea646e3abf9fd8b165124c5ec3a45
highintothesky <highintothesky@gmail.com>
2015-02-22 05:52:04 +0100

    bla

--- sliding_window_detector1.py
+++ sliding_window_detector1.py
@@ -6,6 +6,7 @@
 import numpy as np
 import cv2
 
+from sklearn.preprocessing import normalize
 from sklearn.externals import joblib
 from skimage import color
 from skimage.feature import hog
@@ -14,20 +15,23 @@ from PIL import Image
 
 model_clone = joblib.load('hog_dbn_models/hog_dbn_1.pkl')
 
-path = 'test_images/not2.jpg'
+path = 'test_images/5.gif'
+or_image = Image.open(path)
+image = color.rgb2gray(np.asarray(or_image.resize((320,243))))
 
-image = color.rgb2gray(np.asarray(Image.open(path)))
 
 print(np.shape(image))
 
-image_crop = image[0:243,0:320]
+# image_crop = image[0:243,0:320]
 
-print(np.shape(image_crop))
+# image_res = image.resize((320,243))
 
-fd = np.asarray(hog(image_crop, orientations=9, pixels_per_cell=(16, 16),
+# print(np.shape(image_crop))
+
+fd = np.asarray(hog(image, orientations=9, pixels_per_cell=(16, 16),
                     cells_per_block=(3, 3), visualise=False))
 
-fd_transpose = np.zeros((1,18954), dtype=np.int)
+fd_transpose = np.zeros((1,18954))
 fd_transpose[:,:] = fd
 
 print(fd.shape, fd_transpose.shape)
@@ -36,5 +40,5 @@ pred = model_clone.predict(fd_transpose)
 
 print(pred)
 
-cv2.imshow("crop", image_crop)
+cv2.imshow("crop", image)
 cv2.waitKey(0)

abe7b572c80092d7a2cdb9b1c528555b5ef61043
highintothesky <highintothesky@gmail.com>
2015-04-14 02:10:56 +0200

    v1 finished
    finished sliding window v1

--- sliding_window_detector1.py
+++ sliding_window_detector1.py
@@ -17,7 +17,7 @@ from PIL import Image
 model_clone = joblib.load('hog_dbn_models/hog_dbn_1.pkl')
 
 # test image
-path = 'test_images/1.jpg'
+path = 'test_images/7.gif'
 # or_image = Image.open(path)
 # image = color.rgb2gray(np.asarray(or_image.resize((320,243))))
 image = color.rgb2gray(np.asarray(Image.open(path)))
@@ -35,6 +35,7 @@ print overlap_height, overlap_width
 
 # exit if target is too small to apply sliding windows
 if(target_width < detect_width or target_height < detect_height):
+	print("too small!")
 	sys.exit()
 
 # get the number of possible windows in target image
@@ -42,26 +43,37 @@ hor_windows = int(math.floor(target_width/overlap_width - 1))
 ver_windows = int(math.floor(target_height/overlap_height - 1))
 print hor_windows, ver_windows
 
-for (x in hor_windows - 1):
-	for (y in ver_windows - 1):
-		im_window = image[y*overlap_height:detect_height+y*overlap_height , ]
+for x in range(0, hor_windows):
+	for y in range(0, ver_windows):
+		im_window = image[y*overlap_height:detect_height+y*overlap_height , x*overlap_width:detect_width + x*overlap_width]
+		fd = np.asarray(hog(im_window, orientations=9, pixels_per_cell=(16, 16),
+		                    cells_per_block=(3, 3), visualise=False))
+
+		fd_transpose = np.zeros((1,18954))
+		fd_transpose[:,:] = fd
+
+		pred = model_clone.predict(normalize(fd_transpose))
+		print pred
+		if(pred == [1]):
+			cv2.imshow("hit!", im_window)
+			cv2.waitKey(0)
 
 
 # image_crop = image[0:243,0:320]
 # image_res = image.resize((320,243))
 # print(np.shape(image_crop))
 
-fd = np.asarray(hog(image, orientations=9, pixels_per_cell=(16, 16),
-                    cells_per_block=(3, 3), visualise=False))
+# fd = np.asarray(hog(im_window, orientations=9, pixels_per_cell=(16, 16),
+#                     cells_per_block=(3, 3), visualise=False))
 
-fd_transpose = np.zeros((1,18954))
-fd_transpose[:,:] = fd
+# fd_transpose = np.zeros((1,18954))
+# fd_transpose[:,:] = fd
 
-print(fd.shape, fd_transpose.shape)
+# print(fd.shape, fd_transpose.shape)
 
-pred = model_clone.predict(normalize(fd_transpose))
+# pred = model_clone.predict(normalize(fd_transpose))
 
-print(pred)
+# print(pred)
 
-cv2.imshow("crop", image)
-cv2.waitKey(0)
+# cv2.imshow("crop", image)
+# cv2.waitKey(0)
